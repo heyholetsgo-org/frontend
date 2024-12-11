0c2532ce59fcc160c45b25dea98c6be47e86905b
Robert Honz <exislow+github@gmail.com>
2024-12-11 13:50:33 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
